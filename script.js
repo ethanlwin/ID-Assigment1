ca121abc2df4f94e6660ccc9b30a4f4234054099
ethanlwin <ethan.thutalwin@gmail.com>
2023-12-02 05:12:38 +0800

    hi

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-function showPage(pageId) {
-    // Hide all pages
-    const pages = document.querySelectorAll('.page');
-    for (const page of pages) {
-      page.style.display = 'none';
-    }
-  
-    // Show the specified page or default page
-    if (pageId) {
-      document.getElementById(pageId).style.display = 'block';
-    } else {
-      document.getElementById('home').style.display = 'block';
-    }
+
+// Function to open the sign-in modal
+function openSignInModal() {
+  var modal = document.getElementById('signinModal');
+  modal.style.display = 'flex';
+}
+
+// Function to close the sign-in modal
+function closeSignInModal() {
+  var modal = document.getElementById('signinModal');
+  modal.style.display = 'none';
+}
+
+// Function to handle sign-in
+function signIn(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Retrieve username and password from the form
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  // Perform authentication (add your own logic here)
+  // For simplicity, let's assume a hardcoded username and password
+  if (username === 'ethan' && password === '123') {
+    alert('Sign-in successful!');
+    closeSignInModal();
+    // Change the text of the "Sign In" link to "Account"
+    var signInButton = document.getElementById('signin-link');
+    signInButton.innerHTML = 'Account';
+  } else {
+    alert('Invalid username or password. Please try again.');
   }
-  
-  // Show page 1 initially
-  showPage("home");
-  
+}
+
+
+
+// Function to simulate signing in (replace this with your actual sign-in logic)
+function simulateSignIn() {
+  // Assuming the user has successfully signed in
+  // Change the text of the "Sign In" button to "Account"
+  var signInButton = document.getElementById('signin-link');
+  signInButton.innerHTML = 'Account';
+}
